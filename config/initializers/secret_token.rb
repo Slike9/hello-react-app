5abdb397dff07fbec89c5f675d1156a12fcd5811
Sergey Semyonov <slike9@rambler.ru>
2014-01-11 11:09:50 +0400

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+HelloReactApp::Application.config.secret_key_base = '4f83c42b371fcc396048c41c06b755ff05b897012f8a49f9b4beea8db2b096e5cc91a734a9bfcd6f86f17776cd1b908c7ee882f397023593752e09458817954d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+HelloReactApp::Application.config.session_store :cookie_store, key: '_hello-react-app_session'
