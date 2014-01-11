e64381ad20606a53b146bdf741289da3ba5bdd60
Sergey Semyonov <slike9@rambler.ru>
2014-01-11 19:22:47 +0400

    style, statistics

--- app/assets/javascripts/welcome.js.jsx
+++ app/assets/javascripts/welcome.js.jsx
@@ -13,7 +13,10 @@ var TodoItem = React.createClass({
     onDoneChange: function(e){
         this.props.onItemDoneChange(this.props.item.id, e.target.checked);
     },
-
+    cancelEdit: function(){
+      this.props.item.newText = this.props.item.text;
+      this.props.onItemEditDone(this.props.item.id);
+    },
     renderItem: function(item){
       var cx = React.addons.classSet;
       var classes = cx({
@@ -21,33 +24,40 @@ var TodoItem = React.createClass({
       });
       return item.state == "edit"
         ? <tr>
-            <td colSpan='2'>
+            <td className='item__doneMark'/>
+            <td>
               <form
                 className="form-inline"
                 onSubmit={this.handleSubmit}>
                 <div className="form-group">
                   <input
                     className="form-control"
-                    value={item.text}
+                    value={item.newText}
                     onChange={this.props.onItemChange.bind(null, item.id)}/>
                 </div>
+                <button type='submit' className='btn btn-primary btn-sm' disabled={!this.props.item.newText}>Save</button>
+                <button type='button' className='btn btn-danger btn-sm' onClick={this.cancelEdit}>Cancel</button>
               </form>
             </td>
+            <td className='item__actions'/>
           </tr>
         : <tr className={classes}>
-            <td>
+            <td className='item__doneMark'>
               <input type='checkbox' onChange={this.onDoneChange} checked={item.state == "done"}/>
-              <span className='item__text' onClick={this.props.onItemClick.bind(null, item)}>{item.text}</span>
             </td>
             <td>
+              <span className='item__text' onClick={this.props.onItemClick.bind(null, item)}>{item.text}</span>
+            </td>
+            <td className='item__actions'>
               <div>
                 <button
+                  type='button'
                   className="btn btn-xs btn-success item-done-btn"
                   onClick={this.props.onItemDone.bind(null, this.props.item.id)}>
                     Done
                 </button>
-
                 <button
+                  type='button'
                   className="btn btn-xs btn-danger"
                   onClick={this.props.onItemDelete.bind(null, this.props.item.id)}>
                     Delete
@@ -81,6 +91,30 @@ var TodoList = React.createClass({
     }
 });
 
+var Statistics = React.createClass({
+ render: function(){
+   var items = this.props.items;
+   var doneItemCount = _.where(items, {state: 'done'}).length;
+   return (
+     <table className='table'>
+       <thead>
+         <tr>
+           <th>Active</th>
+           <th>Done</th>
+           <th>All</th>
+         </tr>
+       </thead>
+       <tbody>
+         <tr>
+           <td>{items.length - doneItemCount}</td>
+           <td>{doneItemCount}</td>
+           <td>{items.length}</td>
+         </tr>
+       </tbody>
+   </table>);
+ }
+});
+
 var TodoApp = React.createClass({
     getInitialState: function(){
       return {items: [], text: "", counter: 0};
@@ -99,19 +133,23 @@ var TodoApp = React.createClass({
     onItemClick: function(item){
       if (item.state != "done"){
         item.state = "edit";
+        item.newText = item.text;
         this.setState({items: this.state.items});
       }
     },
 
     onItemChange: function(id, e){
       var item = _.findWhere(this.state.items, {id: id});
-      item.text = e.target.value;
+      item.newText = e.target.value;
       this.setState({items: this.state.items});
     },
 
     onItemEditDone: function(id){
       var item = _.findWhere(this.state.items, {id: id});
       item.state = "active";
+      if (item.newText){
+         item.text = item.newText;
+      }
       this.setState({items: this.state.items});
     },
 
@@ -141,24 +179,29 @@ var TodoApp = React.createClass({
 
     render: function(){
         return (
-          <div>
-            <form className='form-inline' role='form' onSubmit={this.handleSubmit}>
-              <div className='form-group'>
-                <input
-                  placeholder={new_item_placeholder}
-                  className='form-control'
-                  onChange={this.onChange}
-                  value={this.state.text}/>
-              </div>
-              <button className='btn btn-primary'>Submit</button>
-            </form>
-            <TodoList items={this.state.items}
-                onItemClick={this.onItemClick}
-                onItemChange={this.onItemChange}
-                onItemEditDone={this.onItemEditDone}
-                onItemDone={this.onItemDone}
-                onItemDelete={this.onItemDelete}
-                onItemDoneChange={this.onItemDoneChange}/>
+          <div className='row'>
+            <div className='col-xs-6'>
+              <form className='form-inline' role='form' onSubmit={this.handleSubmit}>
+                <div className='form-group'>
+                  <input
+                    placeholder={new_item_placeholder}
+                    className='form-control'
+                    onChange={this.onChange}
+                    value={this.state.text}/>
+                </div>
+                <button type='submit' className='btn btn-primary' disabled={!this.state.text}>Add</button>
+              </form>
+              <TodoList items={this.state.items}
+                  onItemClick={this.onItemClick}
+                  onItemChange={this.onItemChange}
+                  onItemEditDone={this.onItemEditDone}
+                  onItemDone={this.onItemDone}
+                  onItemDelete={this.onItemDelete}
+                  onItemDoneChange={this.onItemDoneChange}/>
+            </div>
+            <div className='col-xs-6'>
+              <Statistics items={this.state.items}/>
+            </div>
           </div>
         );
     }
