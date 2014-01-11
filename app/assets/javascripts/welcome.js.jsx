b7478671e9501dec0e6d77291a8db7dcc0bc60be
soothsayer <d.chernyatiev@gmail.com>
2014-01-11 07:39:38 +0000

    TODO form with input and inline list

--- app/assets/javascripts/welcome.js.jsx
+++ app/assets/javascripts/welcome.js.jsx
@@ -3,9 +3,37 @@
 // All this logic will automatically be available in application.js.
 // You can use CoffeeScript in this file: http://coffeescript.org/
 
+var new_item_placeholder = 'Add text here';
+
 var TodoApp = React.createClass({
+    getInitialState: function(){
+      return {items: [], text: new_item_placeholder};
+    },
+
+    handleSubmit: function(e){
+      e.preventDefault();
+      var new_items = this.state.items.concat([this.state.text]);
+      this.setState({text: new_item_placeholder, items: new_items});
+    },
+
+    onChange: function(e){
+      this.setState({text: e.target.value});
+    },
+
     render: function(){
-        return (<b>Hello</b>);
+        return (
+          <div>
+            <form onSubmit={this.handleSubmit}>
+              <input onChange={this.onChange} value={this.state.text}/>
+              <button>Submit</button>
+            </form>
+            <ul>
+              {this.state.items.map(function(item){
+                return <li>{item}</li>;
+              })}
+            </ul>
+          </div>
+        );
     }
 });
 
